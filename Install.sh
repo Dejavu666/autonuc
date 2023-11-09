ca6faf624a785dbb59310ac095c1dc09b6979ff7
lifanwar <52408309+lifanwar@users.noreply.github.com>
2023-11-09 18:20:03 +0200

    Update Install.sh
    Check instalasi subfinder, nuclei, httpx-toolkit jika tidak ada. Maka, di install

--- Install.sh
+++ Install.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
-# Rename and move autonuc.sh file to /usr/local/bin/autonuc
+# Check if subfinder is installed, if not, install it
+if ! command -v subfinder &> /dev/null; then
+    echo "Installing subfinder..."
+    sudo apt install subfinder
+fi
+
+# Check if httpx is installed, if not, install it
+if ! command -v httpx-toolkit &> /dev/null; then
+    echo "Installing httpx-toolkit..."
+    sudo apt install httpx-toolkit
+fi
+
+# Check if nuclei is installed, if not, install it
+if ! command -v nuclei &> /dev/null; then
+    echo "Installing Nuclei..."
+    go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei@latest
+fi
+
+# Rename and move autonuc.sh file to /usr/bin/autonuc
 sudo mv autonuc.sh /usr/bin/autonuc
 
-# Make the NucleiFuzzer file executable
-sudo chmod u+x /usr/local/bin/autonuc
+# Make the Autonuc file executable
+sudo chmod +x /usr/local/bin/autonuc
 
-# Remove the NucleiFuzzer folder from the home directory
+# Remove the Autonuc folder from the home directory
 if [ -d "$home_dir/Autonuc" ]; then
     echo "Removing Autonuc folder..."
     rm -r "$home_dir/Autonuc"
 fi
 
-echo "NucleiFuzzer has been installed successfully! Now Enter the command 'nucleifuzzer' to run the tool."
+echo "Autonuc has been installed successfully! Now Enter the command 'autonuc' to run the tool."
