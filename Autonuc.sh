a322141cc21e413c9fbb02edeede53be22e21918
lifanwar <52408309+lifanwar@users.noreply.github.com>
2023-11-09 18:28:28 +0200

    Create Autonuc.sh
    Execute subfinder, httpx-toolkit dan nuclei

---
+++ Autonuc.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo 'Silahkan ketikkan url untuk auto scanning'
+
+url=$1
+
+echo 'Sedang scanning active sub domain $url...'
+subfinder -d $url -o subfinder.txt && httpx-toolkit -l subfinder.txt -mc 200 | tee domainact.txt
+
+rm subfinder.txt
+
+echo 'Scanning selesai... file tersimpan subdomainact.txt'
+echo 'Scanning Nuclei...'
+
+nuclei -l domainact.txt -ept ssl -s low,medium,high,critical -o nuclei.txt
